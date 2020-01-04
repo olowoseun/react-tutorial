957978c590f87976b76bca1d8a17283993bbb4c4
Olowogoke Seun <seunolowogoke@gmail.com>
2020-01-04 16:02:10 +0100

    Updates

---
+++ precache-manifest.e6175743cefdd7d941112616e2baaa53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2dcbdae5c7efae86bcede66bdb67075",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "806fa5fac0ed14897ec0",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "ded83ed8af41b76641cf",
+    "url": "/react-tutorial/static/js/2.71a45732.chunk.js"
+  },
+  {
+    "revision": "83b3d04082a8c50f224f3e213e6a5a45",
+    "url": "/react-tutorial/static/js/2.71a45732.chunk.js.LICENSE"
+  },
+  {
+    "revision": "806fa5fac0ed14897ec0",
+    "url": "/react-tutorial/static/js/main.ce7fd9b7.chunk.js"
+  },
+  {
+    "revision": "b7e58e4d450daf8c1e9b",
+    "url": "/react-tutorial/static/js/runtime-main.56908e1a.js"
+  }
+]);
